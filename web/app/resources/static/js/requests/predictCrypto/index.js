73b7db95077dc1a9a6ee6cc3150e3e8294836f41
fabricio covalesci <fabcovalesci@gmail.com>
2022-10-05 08:55:10 -0300

    removido arquivos desnecessarios

--- web/app/resources/static/js/requests/predictCrypto/index.js
+++ web/app/resources/static/js/requests/predictCrypto/index.js
@@ -3,12 +3,9 @@
 
 
 
-
-
 async function get_corr_home() {
     try {
         const resp = await axios.get(`http://127.0.0.1:5000/corr`);
-        console.log('adsaoas');
             if (typeof resp.data !== undefined){
                 return resp.data;
             }
